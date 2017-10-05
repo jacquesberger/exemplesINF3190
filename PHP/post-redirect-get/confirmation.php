7c61e9e32ea95b746c6ca065836f011ba3314f14
Jacques Berger <jacques.berger@code3.ca>
2017-10-05 16:29:55 -0400

    Exemple de POST-REDIRECT-GET

---
+++ PHP/post-redirect-get/confirmation.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Inscription terminée</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <p>Inscription terminée avec succès.</p>
+  </body>
+</html>
---
+++ PHP/post-redirect-get/formulaire.php
@@ -0,0 +1,70 @@
+<?php
+  $message = "";
+  $prenom = "";
+  $nom = "";
+  $succes = false;
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Vérifier le format des données reçues.
+    if (!isset($_POST["prenom"]) || !isset($_POST["nom"]) || !isset($_POST["age"])) {
+      // TODO Valider que la valeur de l'âge est numérique et dans les valeurs 
+      // acceptées.
+      http_response_code(400);
+      exit;
+    }
+
+    $prenom = $_POST["prenom"];
+    $nom = $_POST["nom"];
+    $age = intval($_POST["age"]);
+
+    if ($prenom == "") {
+      $message = $message . "Le prénom est obligatoire. ";
+    }
+    if ($nom == "") {
+      $message = $message . "Le nom est obligatoire. ";
+    }
+
+    if ($message == "") {
+      header("Location: confirmation.php", true, 303);
+      exit;
+    } 
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Inscription</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h2>Formulaire d'inscription</h2>
+    <?php if ($message != "") {
+            echo "<p>{$message}</p>";
+          }
+     ?>
+    <form method="post" action="formulaire.php">
+      <label for="champs-nom">Nom : </label>
+      <?php echo "<input type='text' name='nom' id='champs-nom' value='{$nom}'>"; ?>
+      <br>
+      <label for="champs-prenom">Prénom : </label>
+      <?php echo "<input type='text' name='prenom' id='champs-prenom' value='{$prenom}'>"; ?>
+      <br>
+      <label for="champs-age">Âge : </label>
+      <select name="age" id="champs-age">
+        <?php
+          for($i = 20; $i < 25; $i++) {
+            if ($i == $age) {
+              $selected = "selected='selected'";
+            } else {
+              $selected = "";
+            }
+            echo "<option value='{$i}' {$selected}>{$i}</option>";
+          }
+        ?>
+      </select>
+      <br>
+      <br>
+      <input type="submit" value="S'inscrire">
+    </form>
+  </body>
+</html>
